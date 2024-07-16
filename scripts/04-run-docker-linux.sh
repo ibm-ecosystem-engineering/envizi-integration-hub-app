a0e1daf740214bc446b8e326d49716cd66a560dd
Jeya Gandhi Rajan <GandhiCloudLab@gmail.com>
2024-07-16 17:14:48 +0530

    update

--- scripts/04-run-docker-linux.sh
+++ scripts/04-run-docker-linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run -d -p 3001:3001 --name my-e-int-hub \
+docker run -d -p 3001:3001 --name my-e-int2-hub \
     --env LOGLEVEL=DEBUG \
      --env DATA_FOLDER=/app/data \
      --env DATA_STORE_FOLDER=/app/data \
@@ -16,9 +16,6 @@ podman run -d -p 3001:3001 --name my-e-int2-hub \
     --env OUTPUT_FOLDER="/app/output" \
     -v "/root/int-hub/envizi-config.json:/app/envizi-config.json" \
     gandigit/e-int-hub2-linux:latest
-
-
-podman run -d -p 3000:3000 --name my-e-int-hub2-ui --env NEXT_PUBLIC_API_URL="http://9.30.230.165:3001"  gandigit/e-int-hub2-ui-linux:latest
 
 
 echo "build completed ...."
--- scripts/23-run-dockerhub-ui.sh
+++ scripts/23-run-dockerhub-ui.sh
@@ -6,6 +6,6 @@ DOCKER_IMAGE=gandigit/e-int-hub2-ui-linux
 
 cd ../web
 
-podman run -d -p 3000:3000 --name my-e-int-hub2-ui $DOCKER_IMAGE:latest
+podman run -d -p 3000:3000 --name my-e-int-hub2-ui --env NEXT_PUBLIC_API_URL="http://1.2.3.4:3001"  $DOCKER_IMAGE:latest
 
 echo "build completed ...."
